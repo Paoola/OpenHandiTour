40cbeee20a36cddc35134a740694a89e2e1e491a
Xavier HAUSHERR <xavier@e-zic.com>
2018-10-12 23:36:56 +0200

    Add ReadMe

--- src/Graph/Type/DatetimeType.php
+++ src/Graph/Type/DatetimeType.php
@@ -2,11 +2,7 @@
 
 namespace App\Graph\Type;
 
-use GraphQL\Language\AST\Node;
-use GraphQL\Type\Definition\ScalarType;
-use Overblog\GraphQLBundle\Definition\Resolver\AliasedInterface;
-
-class DatetimeType extends ScalarType implements AliasedInterface
+class DatetimeType extends DateType
 {
     /**
      * @param \DateTime $value
@@ -16,26 +12,6 @@ class DatetimeType extends ScalarType implements AliasedInterface
     public function serialize($value)
     {
         return $value->format('c');
-    }
-
-    /**
-     * @param mixed $value
-     *
-     * @return mixed
-     */
-    public function parseValue($value)
-    {
-        return new \DateTime($value);
-    }
-
-    /**
-     * @param Node $valueNode
-     *
-     * @return string
-     */
-    public function parseLiteral($valueNode, array $variables = null)
-    {
-        return $this->parseValue($valueNode->value);
     }
 
     public static function getAliases()
